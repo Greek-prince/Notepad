68be510d50f5cb03aedad84deaa8d5a9b6c96e30
Cray Fish <cray.fish.75.02@gmail.com>
2025-01-12 14:27:56 +0530

    update delete changes with ui change

--- index.js
+++ index.js
@@ -1,51 +1,127 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 const fs = require('fs');
-app.set("view engine","ejs");
+
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
-app.get('/',function(req,res){
-    fs.readdir(`./files`,function(err,files){//readdir basically helps us to read any type of files or directory
-        res.render("index",{files:files});    
-    })
-    
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/files/:filename',function(req,res){
-    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err, filedata){
-        //readdir basically helps us to read any type of files or directory
-        //if (err) {
-           // return res.status(500).send("Error reading file");
-        //}
-        res.render("show",{filename:req.params.filename, filedata: filedata});
-        //console.log(filedata);
-        
-        
-    })
+// Home route to display the list of files
+app.get('/', function (req, res) {
+    fs.readdir('./files', function (err, files) {
+        if (err) {
+            return res.status(500).send('Error reading files');
+        }
+        res.render("index", { files: files });
+    });
+});
+
+// Route to display the content of a selected file
+app.get('/files/:filename', function (req, res) {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', function (err, filedata) {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+        fs.readdir('./files', function (err, files) {
+            res.render('show', {
+                filename: req.params.filename,
+                filedata: filedata,
+                files: files // Pass the list of files to the view
+            });
+        });
+    });
+});
+
+// Route to render the edit form for renaming or editing a task
+app.get('/edit/:filename', function (req, res) {
+    const filePath = `./files/${req.params.filename}`;
+    fs.readFile(filePath, 'utf-8', function (err, filedata) {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+        res.render('edit', {
+            filename: req.params.filename,
+            description: filedata // Pass file data to description
+        });
+    });
+});
+
+// Route to handle renaming or updating description
+app.post('/edit', function (req, res) {
+    const { previous, new: newName, description } = req.body;
+
+    const filePath = `./files/${previous}`;
+    console.log(`./files/${newName}`);
     
-})
-app.get('/edit/:filename',function(req,res){
-    res.render('edit',{filename:req.params.filename});
-   
-    
-})
-app.post('/edit',function(req,res){
-    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+    console.log('Trying to edit file at path:', filePath);  // Debugging line
+
+    fs.access(filePath, fs.constants.F_OK, function (err) {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+
+        // Read the current file to get the content
+        fs.readFile(filePath, 'utf-8', function (err, filedata) {
+            if (err) {
+                return res.status(404).send('Error reading the file');
+            }
+
+            // Determine if the description has changed
+            let updatedContent = filedata;
+            if (description) updatedContent = description;
+
+            // If a new name is provided, rename the file
+            if (newName) {
+                fs.rename(filePath, `./files/${newName}`, function (err) {
+                    if (err) {
+                        return res.status(500).send('Error renaming the file');
+                    }
+                    // Write the updated content to the new file
+                    fs.writeFile(`./files/${newName}`, updatedContent, function (err) {
+                        if (err) {
+                            return res.status(500).send('Error updating the file');
+                        }
+                        res.redirect('/');
+                    });
+                });
+            } else {
+                // If no new name, just update the content
+                fs.writeFile(filePath, updatedContent, function (err) {
+                    if (err) {
+                        return res.status(500).send('Error updating the file');
+                    }
+                    res.redirect('/');
+                });
+            }
+        });
+    });
+});
+
+// Route to create a new file
+app.post('/create', function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+        if (err) {
+            return res.status(500).send('Error creating the file');
+        }
         res.redirect("/");
+    });
+});
 
-    })
-})
-app.post('/create',function(req,res){//because we already declear the method is post so get is not used
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){//readdir basically helps us to read any type of files or directory
-        res.redirect("/");    
-    })
-    
-})
-app.listen(3000);
-//in ejs we atfirst done the background pitch black colour by zinc 900
-//next we create form and tasks
-//next we use fs module 
-//next we create a folder and attach it with ejs
-//atnext we create a loop if there is no task then no task shown if there is then some task is here added
+// Route to delete a file
+app.post('/delete', function (req, res) {
+    const fileToDelete = req.body.filename;
+
+    fs.unlink(`./files/${fileToDelete}`, function (err) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Error deleting file');
+        }
+        res.redirect('/');
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Server is running on port 3000');
+});
